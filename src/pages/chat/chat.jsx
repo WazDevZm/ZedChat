bdd6c768de2aec43f4b477c9ec152e2b0e60ee64
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-06-08 23:22:46 +0200

    add login page with form and styling

--- src/pages/chat/chat.jsx
+++ src/pages/chat/chat.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import "./chat.css"
 // leanrinnng ow to code this wayis very fun, i like tit
-
+// building and leanig in real time somthing really cool
+// this is a chat app that is bing built using react and selected
+// backedn called firebase by googble, making the comments f
 const chat = () => {
   return (
     <div>
@@ -11,3 +13,5 @@ const chat = () => {
 }
 
 export default chat
+// this is why in need a second sccren to se my ode nad the 
+// actual work that I im imomeing by the side, me buildin th next facbook
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -7,10 +7,23 @@ const Login = () => {
     <div>
       <div className='login'>
         <img src={assets.logo_big} alt='' className='logo' />
+        <form className="login-form">
+          <h2>Sign Up</h2> 
+          <input type="text" placeholder="username" className="form-input" required />
+          <input type="email" placeholder="email" className="form-input" required/>
+          <input type="password" placeholder="password" className="form-input" required/>
+          <button type="submit" className="btn">Sign Up</button>
+          <div className="login-term">
+            <input type='checkbox' id='terms'/>
+            <label htmlFor='terms'>Agree to the terms of use & privacy policy.</label>
+          </div>
+          <div className='login-forgot'>
+            <p className='login-toggle'>Already have an account? <span>Login</span></p>
+          </div>
+        </form>
       </div>
     </div>
   )
 }
 
 export default Login
-// building  first of it's kind chart app using react
