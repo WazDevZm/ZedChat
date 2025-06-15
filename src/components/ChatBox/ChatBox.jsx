7bb6d8f3d5a1385a6b92fe80374aa503fcd749d6
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-06-15 20:06:48 +0200

    feat: implement ChatBox component with user interaction and styling; add logo asset

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './ChatBox.css'
+import assets from '../../assets/assets'
+
 
 const ChatBox = () => {
   return (
-    <div>
-      
+    <div className='chat-box'>
+      <div className='chat-user'>
+        <img src={assets.profile_img} alt='' />
+        <p>Wazingwa Mugala <img className='dot' src={assets.green_dot} alt='' /></p>
+        <img src={assets.help_icon} className='help' alt=''/>
+      </div>
+      <div className="chat-input">
+        <input type='text' placeholder='send message' />
+        <input type='file' id='image' accept='image/png, imgage/jpg' hidden />
+        <label htmlFor='image'>
+          <img src={assets.gallery_icon} alt='' />
+        </label>
+        <img src={assets.send_button} alt='' />
+      </div>
+
     </div>
   )
 }
